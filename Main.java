20e17e4f5c7ae8b3ecdca1fb9c5cd7877daa1579
Hager Samir <40361975+hagersamir123@users.noreply.github.com>
2024-07-09 09:41:13 +0300

    add main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+class MyPanel extends JPanel{
+
+	public void paint(Graphics g){
+		super.paint(g);
+		g.drawString("Hello", 100, 100);
+	}
+
+}
+
+public class Main{
+	public static void main (String[] args){
+		
+		JFrame frame = new JFrame();
+		frame.setContentPane(new MyPanel());
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+	}
+
+}
+
